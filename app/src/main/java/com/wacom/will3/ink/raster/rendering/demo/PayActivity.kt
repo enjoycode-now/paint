adec237c30db4ecb6a1d0e5d7c052c62a5c6898e
chen <1356431628@qq.com>
2022-01-24 15:28:59 +0800

    import Wechat pay and three button

--- app/src/main/java/com/wacom/will3/ink/raster/rendering/demo/PayActivity.kt
+++ app/src/main/java/com/wacom/will3/ink/raster/rendering/demo/PayActivity.kt
@@ -13,12 +13,12 @@ import android.content.Intent
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
+
+
 import com.tencent.mm.opensdk.constants.ConstantsAPI
-import com.tencent.mm.opensdk.modelpay.JumpToOfflinePay
 import com.tencent.mm.opensdk.modelpay.PayReq
-
-
-
+import com.wacom.will3.ink.raster.rendering.demo.wxapi.WXPayEntryActivity
+import com.wacom.will3.ink.raster.rendering.demo.wxapi.util.WXConstants
 
 
 class PayActivity : AppCompatActivity() , IWXAPIEventHandler {
@@ -32,7 +32,7 @@ class PayActivity : AppCompatActivity() , IWXAPIEventHandler {
         api = WXAPIFactory.createWXAPI(this, null)
 
         // 将应用的appId注册到微信
-        api.registerApp(Constants.APP_ID)
+        api.registerApp(WXConstants.APP_ID)
 
         try {
             api.handleIntent(intent, this)
@@ -71,22 +71,20 @@ class PayActivity : AppCompatActivity() , IWXAPIEventHandler {
         }
     }
 
-    class Constants {
-        companion object {
-            val APP_ID = "yourAPP_id"
-        }
-
-    }
 
     fun pay(view: View) {
+
+        //调起支付
         val request = PayReq()
-        request.appId = "wxd930ea5d5a258f4f"
-        request.partnerId = "1900000109"
-        request.prepayId = "1101000000140415649af9fc314aa427"
-        request.packageValue = "Sign=WXPay"
-        request.nonceStr = "1101000000140429eb40476f8896f4c9"
-        request.timeStamp = "1398746574"
-        request.sign = "7FFECB600D7157C5AA49810D2D8F28BC2811827B"
+        request.appId = WXConstants.APP_ID
+        request.partnerId = ""
+        request.prepayId = ""
+        request.packageValue = ""
+        request.nonceStr = ""
+        request.timeStamp = ""
+        request.sign = ""
         api.sendReq(request)
+        startActivity(Intent(this,WXPayEntryActivity::class.java))
     }
+
 }
