198caaeb7ef0dbc5ab00c46585f23ec67c014543
Fan <g39088902@gmail.com>
2022-01-25 11:48:00 +0800

    change package name

--- app/src/main/java/com/wacom/will3/ink/raster/rendering/demo/utils/BitmapUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.wacom.will3.ink.raster.rendering.demo.utils
-
-import android.graphics.Bitmap
-
-object BitmapUtils {
-    fun newBitmap(width:Int=1200,height:Int=1800) = Bitmap.createBitmap(width,height, Bitmap.Config.ARGB_8888)
-}
--- app/src/main/java/com/wacom/will3/ink/raster/rendering/demo/wxapi/util/WXConstants.kt
+++
@@ -1,8 +0,0 @@
-package com.wacom.will3.ink.raster.rendering.demo.wxapi.util
-
-public class WXConstants {
-
-    companion object{
-        val APP_ID : String = "这里替换公司项目微信开放平台的APP_ID"
-    }
-}
