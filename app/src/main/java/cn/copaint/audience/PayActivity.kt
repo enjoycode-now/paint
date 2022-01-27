78254beb30238826b04b701dc9fdbac054d8f86c
chen <1356431628@qq.com>
2022-01-27 15:46:20 +0800

    rollback and cancel mask

--- app/src/main/java/cn/copaint/audience/PayActivity.kt
+++ app/src/main/java/cn/copaint/audience/PayActivity.kt
@@ -27,10 +27,7 @@ import cn.copaint.audience.wxapi.WXPayEntryActivity
 import com.alipay.sdk.app.PayTask
 
 
-
-
-
-class PayActivity : AppCompatActivity() , IWXAPIEventHandler {
+class PayActivity : AppCompatActivity(), IWXAPIEventHandler {
 
     /**
      * 用于支付宝支付业务的入参 app_id。
@@ -96,7 +93,7 @@ class PayActivity : AppCompatActivity() , IWXAPIEventHandler {
             } else {
                 Toast.makeText(this, "支付失败", Toast.LENGTH_LONG).show()
             }
-            val builder= AlertDialog.Builder(this)
+            val builder = AlertDialog.Builder(this)
             builder.setTitle("提示")
         }
     }
@@ -104,7 +101,7 @@ class PayActivity : AppCompatActivity() , IWXAPIEventHandler {
     /**
      * 向服务器申请下单
      * */
-    fun createOrder(){
+    fun createOrder() {
 
     }
 
@@ -120,14 +117,15 @@ class PayActivity : AppCompatActivity() , IWXAPIEventHandler {
         request.timeStamp = ""
         request.sign = ""
         api.sendReq(request)
-        startActivity(Intent(this,WXPayEntryActivity::class.java))
+        startActivity(Intent(this, WXPayEntryActivity::class.java))
     }
 
     /**
      * 从后台获取预付单数据，唤醒支付宝app支付
      * */
-    fun AliPay(view : View){
-        val orderInfo : String = "alipay_sdk=alipay-sdk-java-4.5.0.ALL&app_id=2017062007529139&biz_content=%7B%22body%22%3A%22%E8%B4%AD%E4%B9%B0%E4%BA%86%E9%87%91%E5%B8%81%E7%9A%84%E8%B4%B9%E7%94%A8%22%2C%22out_trade_no%22%3A%22c41b8bc484414a908096bfa154addfbe%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22%E9%87%91%E5%B8%81400%E4%B8%AA%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.03%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2F118.178.238.32%3A7074%2FpayAsyncNotify%2FgoldAlipay&sign=cLKHO69X4xEXwlOz7IP7c1tiQERlP1S7yk8UAk1g1WDlNchxjj1YnK01KvsYNSx1Vpj%2BT%2F98LkK6areKcNj4qlPBuG0g3l%2BK%2BpH5n1k7Ejj%2BrPw9VdlSXxeUNlw8eBhvf2yqHTpLT5v0GN62KulllXvX7skDB3Q6n8SDls3kJ6ZJVWDJDNWefRVLfwCg13kK5Uzv0tNifEqr4ii%2Bv1e27TA4ceoT0EHHg0pASmhSCVAj8g3QnpJZNXSL5mnh4WVyq6ka37IL1PVTYJwreN2iNFVfRHUPg4yf16qbjVTN7Y%2FuJpx2X95%2Fn7SpIXcaPoyvkdzHM2CEkxsqqE%2Fcs8w5lA%3D%3D&sign_type=RSA2&timestamp=2020-09-20+20%3A35%3A43&version=1.0" // 订单信息
+    fun AliPay(view: View) {
+        val orderInfo: String =
+            "alipay_sdk=alipay-sdk-java-4.5.0.ALL&app_id=2017062007529139&biz_content=%7B%22body%22%3A%22%E8%B4%AD%E4%B9%B0%E4%BA%86%E9%87%91%E5%B8%81%E7%9A%84%E8%B4%B9%E7%94%A8%22%2C%22out_trade_no%22%3A%22c41b8bc484414a908096bfa154addfbe%22%2C%22product_code%22%3A%22QUICK_MSECURITY_PAY%22%2C%22subject%22%3A%22%E9%87%91%E5%B8%81400%E4%B8%AA%22%2C%22timeout_express%22%3A%2230m%22%2C%22total_amount%22%3A%220.03%22%7D&charset=utf-8&format=json&method=alipay.trade.app.pay&notify_url=http%3A%2F%2F118.178.238.32%3A7074%2FpayAsyncNotify%2FgoldAlipay&sign=cLKHO69X4xEXwlOz7IP7c1tiQERlP1S7yk8UAk1g1WDlNchxjj1YnK01KvsYNSx1Vpj%2BT%2F98LkK6areKcNj4qlPBuG0g3l%2BK%2BpH5n1k7Ejj%2BrPw9VdlSXxeUNlw8eBhvf2yqHTpLT5v0GN62KulllXvX7skDB3Q6n8SDls3kJ6ZJVWDJDNWefRVLfwCg13kK5Uzv0tNifEqr4ii%2Bv1e27TA4ceoT0EHHg0pASmhSCVAj8g3QnpJZNXSL5mnh4WVyq6ka37IL1PVTYJwreN2iNFVfRHUPg4yf16qbjVTN7Y%2FuJpx2X95%2Fn7SpIXcaPoyvkdzHM2CEkxsqqE%2Fcs8w5lA%3D%3D&sign_type=RSA2&timestamp=2020-09-20+20%3A35%3A43&version=1.0" // 订单信息
 
         val payRunnable = Runnable {
             val alipay = PayTask(this@PayActivity)
@@ -182,11 +180,9 @@ class PayActivity : AppCompatActivity() , IWXAPIEventHandler {
                         // 获取alipay_open_id，调支付时作为参数extern_token 的value
                         // 传入，则支付账户为该授权账户
                         toast("支付成功" + authResult)
-
-
                     } else {
                         // 其他状态值则为授权失败
-                        toast("支付成功"  + authResult)
+                        toast("支付失败" + authResult)
                     }
                 }
                 else -> {}
