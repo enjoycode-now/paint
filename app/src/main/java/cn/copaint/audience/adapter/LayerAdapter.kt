182960d77fd699e079fcd22c1c6ee8e48d21be56
Fan <g39088902@gmail.com>
2022-02-02 17:08:26 +0800

    code review

--- app/src/main/java/cn/copaint/audience/adapter/LayerAdapter.kt
+++ app/src/main/java/cn/copaint/audience/adapter/LayerAdapter.kt
@@ -1,6 +1,5 @@
 package cn.copaint.audience.adapter
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -9,8 +8,7 @@ import cn.copaint.audience.R
 import cn.copaint.audience.databinding.ItemLayerSmallBinding
 import cn.copaint.audience.model.RoomLayer
 
-class LayerAdapter(private val activity: DrawActivity) :
-    RecyclerView.Adapter<LayerAdapter.ViewHolder>() {
+class LayerAdapter(private val activity: DrawActivity) : RecyclerView.Adapter<LayerAdapter.ViewHolder>() {
 
     override fun getItemViewType(position: Int) = if (position == activity.layerPos) 0 else 1 // 0:选中,1:未选中
 
@@ -25,38 +23,33 @@ class LayerAdapter(private val activity: DrawActivity) :
 
     override fun getItemCount() = activity.smallLayers.size
 
-    class ViewHolder(val bind: ItemLayerSmallBinding) : RecyclerView.ViewHolder(bind.root) {
+    class ViewHolder(val itemBind: ItemLayerSmallBinding) : RecyclerView.ViewHolder(itemBind.root) {
         var viewType: Int = 1
-        @SuppressLint("SetTextI18n")
         fun bind(roomLayer: RoomLayer, pos: Int, activity: DrawActivity) {
-            bind.layerImage.setImageBitmap(roomLayer.bitmap)
-            bind.layerNameText.text = "图层${pos + 1}"
-            bind.textRight.text = "${String.format("%.0f",roomLayer.alpha * 100 / 255f)}%"
+            itemBind.layerImage.setImageBitmap(roomLayer.bitmap)
+            itemBind.layerNameText.text = "图层${pos + 1}"
+            itemBind.textRight.text = "${String.format("%.0f",roomLayer.alpha * 100 / 255f)}%"
 
-            bind.layerItem.setOnClickListener {
-                with(activity) {
+            with(activity) {
+                itemBind.layerItem.setOnClickListener {
                     if (layerPos != pos) changeToLayer(pos) else layerToolPopupWindow(it)
                 }
-            }
-            if (viewType == 0) bind.layerItem.setBackgroundColor(activity.resources.getColor(R.color.blue, null))
+                if (viewType == 0) itemBind.layerItem.setBackgroundColor(resources.getColor(R.color.blue, null))
 
-            bind.imageLeft.setImageResource(
-                when (activity.smallLayers[pos].isShow) {
-                    true -> R.drawable.ic_icon_xs
-                    else -> R.drawable.ic_icon_yc
-                }
-            )
+                itemBind.imageLeft.setImageResource(
+                    when (smallLayers[pos].isShow) {
+                        true -> R.drawable.ic_icon_xs
+                        else -> R.drawable.ic_icon_yc
+                    }
+                )
 
-            bind.imageLeft.setOnClickListener {
-                with(activity) {
+                itemBind.imageLeft.setOnClickListener {
                     smallLayers[pos].isShow = !smallLayers[pos].isShow
                     layerAdapter.notifyItemChanged(pos)
                     bind.rasterView.refreshView()
                 }
-            }
 
-            bind.textRight.setOnClickListener {
-                with(activity) {
+                itemBind.textRight.setOnClickListener {
                     if (layerPos != pos) changeToLayer(pos) else layerToolPopupWindow(it)
                 }
             }
