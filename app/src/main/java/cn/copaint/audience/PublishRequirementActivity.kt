e9db3ab550e24fdd2ec3d8e0a08e901df6422d96
chen <1356431628@qq.com>
2022-03-01 09:02:08 +0800

    add： 新增图片选择器，支持多选、原图、预览、相机拍摄，新增多项权限申请需求。

--- app/src/main/java/cn/copaint/audience/PublishRequirementActivity.kt
+++ app/src/main/java/cn/copaint/audience/PublishRequirementActivity.kt
@@ -1,29 +1,219 @@
 package cn.copaint.audience
 
-import android.R
-import android.graphics.Color
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import cn.copaint.audience.adapter.GridImageAdapter
 import cn.copaint.audience.databinding.ActivityPublishRequirementBinding
+import cn.copaint.audience.utils.GlideEngine
 import cn.copaint.audience.utils.StatusBarUtils
+import cn.copaint.audience.utils.ToastUtils.toast
+import com.luck.picture.lib.app.PictureAppMaster
+import com.luck.picture.lib.basic.PictureSelectionModel
+import com.luck.picture.lib.basic.PictureSelector
+import com.luck.picture.lib.config.PictureMimeType
+import com.luck.picture.lib.config.PictureSelectionConfig.selectorStyle
+import com.luck.picture.lib.config.SelectMimeType
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.interfaces.OnExternalPreviewEventListener
+import com.luck.picture.lib.interfaces.OnResultCallbackListener
+import com.luck.picture.lib.style.PictureSelectorStyle
+import com.luck.picture.lib.utils.MediaUtils
 
 
 class PublishRequirementActivity : AppCompatActivity() {
+    private val TAG = "chenlin"
     lateinit var binding: ActivityPublishRequirementBinding
+    val photoList = arrayListOf<LocalMedia>()
+    lateinit var mAdapter: GridImageAdapter
+    val maxSelectNum = 10
+    lateinit var launcherResult: ActivityResultLauncher<Intent>
+    val permissionList = arrayListOf(
+        "android.permission.MOUNT_UNMOUNT_FILESYSTEMS",
+        "android.permission.WRITE_SETTINGS",
+        "android.permission.WRITE_MEDIA_STORAGE"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPublishRequirementBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        StatusBarUtils.initSystemBar(window, "#FAFBFF", true)
+        selectorStyle = PictureSelectorStyle()
+        // 注册需要写在onCreate或Fragment onAttach里，否则会报java.lang.IllegalStateException异常
+        launcherResult = createActivityResultLauncher()
 
-        StatusBarUtils.initSystemBar(window,"#FAFBFF",true)
+
+        binding.imageView8.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        mAdapter = GridImageAdapter(this, photoList)
+        binding.imageView8.adapter = mAdapter
+
+        mAdapter.setOnItemClickListener(object : GridImageAdapter.OnItemClickListener {
+            override fun onItemClick(v: View?, position: Int) {
+                // 预览图片、视频、音频
+                PictureSelector.create(this@PublishRequirementActivity)
+                    .openPreview()
+                    .setImageEngine(GlideEngine)
+                    .setSelectorUIStyle(selectorStyle)
+                    .isPreviewFullScreenMode(true)
+                    .setExternalPreviewEventListener(object : OnExternalPreviewEventListener {
+                        override fun onPreviewDelete(position: Int) {
+                            mAdapter.remove(position)
+                            mAdapter.notifyItemRemoved(position)
+                        }
+
+                        override fun onLongPressDownload(media: LocalMedia): Boolean {
+                            return false
+                        }
+                    })
+                    .startActivityPreview(position, true, mAdapter.getData())
+            }
+
+            override fun openPicture() {
+                val mode: Boolean = true
+                if (mode) {
+
+                    // 进入相册
+                    val selectionModel: PictureSelectionModel =
+                        PictureSelector.create(this@PublishRequirementActivity)
+                            .openGallery(SelectMimeType.ofImage())
+                            .setSelectorUIStyle(selectorStyle)
+                            .setImageEngine(GlideEngine)
+                            .isDisplayTimeAxis(true)//显示资源时间轴
+                            .isPageStrategy(false)//是否分页
+                            .isOriginalControl(true)//是否开启原图功能
+                            .isDisplayCamera(true)//是否支持相机拍摄
+                            .isFastSlidingSelect(true) //是否支持滑动选择
+                            .isWithSelectVideoImage(false)//是否支持图片视频同选
+                            .isPreviewFullScreenMode(true)//全屏预览
+                            .isPreviewZoomEffect(true)//预览缩放效果
+                            .isPreviewImage(true)//是否预览图片
+                            .isPreviewVideo(false)
+                            .isPreviewAudio(false)
+                            .isMaxSelectEnabledMask(true)//是否开启蒙版
+                            .setMaxSelectNum(maxSelectNum)//多选模式下最大数量
+                            .isGif(true)//是否显示gif图
+                            .setSelectedData(mAdapter.getData())
+
+                    selectionModel.forResult(launcherResult)
+
+
+                }
+            }
+        })
+
+
+        // 清除缓存
+//        clearCache()
+
+    }
+
+
+
+
+
+
+    /**
+     * 创建一个ActivityResultLauncher
+     *
+     * @return
+     */
+    private fun createActivityResultLauncher(): ActivityResultLauncher<Intent> {
+        return registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            object : ActivityResultCallback<ActivityResult?> {
+
+
+                override fun onActivityResult(result: ActivityResult?) {
+                    val resultCode = result?.resultCode
+                    if (resultCode == RESULT_OK) {
+                        val selectList = PictureSelector.obtainSelectorList(result.data)
+                        analyticalSelectResults(selectList)
+                    } else if (resultCode == RESULT_CANCELED) {
+                        Log.i("chenlin", "onActivityResult PictureSelector Cancel")
+                    }
+                }
+            })
+    }
+
+    /**
+     * 处理选择结果
+     *
+     * @param result
+     */
+    private fun analyticalSelectResults(result: java.util.ArrayList<LocalMedia>) {
+        for (media in result) {
+            if (media.width == 0 || media.height == 0) {
+                if (PictureMimeType.isHasImage(media.mimeType)) {
+                    val imageExtraInfo = MediaUtils.getImageSize(media.path)
+                    media.width = imageExtraInfo.width
+                    media.height = imageExtraInfo.height
+                } else if (PictureMimeType.isHasVideo(media.mimeType)) {
+                    val videoExtraInfo = MediaUtils.getVideoSize(
+                        PictureAppMaster.getInstance().appContext,
+                        media.path
+                    )
+                    media.width = videoExtraInfo.width
+                    media.height = videoExtraInfo.height
+                }
+            }
+            Log.i(TAG, "文件名: " + media.fileName)
+            Log.i(TAG, "是否压缩:" + media.isCompressed)
+            Log.i(TAG, "压缩:" + media.compressPath)
+            Log.i(TAG, "原图:" + media.path)
+            Log.i(TAG, "绝对路径:" + media.realPath)
+            Log.i(TAG, "是否裁剪:" + media.isCut)
+            Log.i(TAG, "裁剪:" + media.cutPath)
+            Log.i(TAG, "是否开启原图:" + media.isOriginal)
+            Log.i(TAG, "原图路径:" + media.originalPath)
+            Log.i(TAG, "沙盒路径:" + media.sandboxPath)
+            Log.i(TAG, "原始宽高: " + media.width + "x" + media.height)
+            Log.i(TAG, "裁剪宽高: " + media.cropImageWidth + "x" + media.cropImageHeight)
+            Log.i(TAG, "文件大小: " + media.size)
+        }
+        runOnUiThread {
+            val isMaxSize = result.size == mAdapter.selectMax
+            val oldSize = mAdapter.list.size
+            mAdapter.notifyItemRangeRemoved(0, if (isMaxSize) oldSize + 1 else oldSize)
+            mAdapter.list.clear()
+            mAdapter.list.addAll(result)
+            mAdapter.notifyItemRangeInserted(0, result.size)
+        }
     }
 
     fun onBackPress(view: View) {
         finish()
     }
 
+    fun onclick(view: View) {
+        PictureSelector.create(this)
+            .openGallery(SelectMimeType.ofImage())
+            .setImageEngine(GlideEngine)
+            .forResult(object : OnResultCallbackListener<LocalMedia?> {
+                override fun onResult(result: ArrayList<LocalMedia?>?) {
+                    result?.forEach {
+                        if (it != null) {
+                            photoList.add(it)
+                        }
+                    }
+                }
+
+                override fun onCancel() {
+                    toast("你已经退出")
+                }
+            })
+    }
 
 
 }
---
+++ app/src/main/java/cn/copaint/audience/adapter/GridImageAdapter.kt
@@ -0,0 +1,232 @@
+package cn.copaint.audience.adapter
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cn.copaint.audience.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.luck.picture.lib.adapter.holder.PreviewGalleryAdapter
+import com.luck.picture.lib.config.PictureMimeType
+import com.luck.picture.lib.config.SelectMimeType
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.utils.DateUtils
+import java.io.File
+
+class GridImageAdapter(context: Context?, result: List<LocalMedia>?) :
+    RecyclerView.Adapter<GridImageAdapter.ViewHolder>() {
+    val TAG = "PictureSelector"
+    private val TYPE_CAMERA = 1
+    val TYPE_PICTURE = 2
+    var mInflater: LayoutInflater? = null
+    val list = ArrayList<LocalMedia>()
+    var selectMax = 10
+
+    /**
+     * 删除
+     */
+    fun delete(position: Int) {
+        try {
+            if (position != RecyclerView.NO_POSITION && list.size > position) {
+                list.removeAt(position)
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position, list.size)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    init{
+        mInflater = LayoutInflater.from(context)
+        list.addAll(result!!)
+    }
+
+
+
+    fun getData(): ArrayList<LocalMedia>? {
+        return list
+    }
+
+    fun remove(position: Int) {
+        if (position < list.size) {
+            list.removeAt(position)
+        }
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var mImg: ImageView
+        var mIvDel: ImageView
+        var tvDuration: TextView
+
+        init {
+            mImg = view.findViewById(R.id.fiv)
+            mIvDel = view.findViewById(R.id.iv_del)
+            tvDuration = view.findViewById(R.id.tv_duration)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return if (list.size < selectMax) {
+            list.size + 1
+        } else {
+            list.size
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (isShowAddItem(position)) {
+            TYPE_CAMERA
+        } else {
+            TYPE_PICTURE
+        }
+    }
+
+    /**
+     * 创建ViewHolder
+     */
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val view: View = mInflater?.inflate(R.layout.item_select_pic, viewGroup, false)!!
+        return ViewHolder(view)
+    }
+
+    private fun isShowAddItem(position: Int): Boolean {
+        val size = list.size
+        return position == size
+    }
+
+    /**
+     * 设置值
+     */
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        //少于MaxSize张，显示继续添加的图标
+        if (getItemViewType(position) == TYPE_CAMERA) {
+            viewHolder.mImg.setImageResource(R.drawable.add_example_pic)
+            viewHolder.mImg.setOnClickListener {
+                if (mItemClickListener != null) {
+                    mItemClickListener!!.openPicture()
+                }
+            }
+            viewHolder.mIvDel.visibility = View.INVISIBLE
+        } else {
+            viewHolder.mIvDel.visibility = View.VISIBLE
+            viewHolder.mIvDel.setOnClickListener { view: View? ->
+                val index = viewHolder.absoluteAdapterPosition
+                if (index != RecyclerView.NO_POSITION && list.size > index) {
+                    list.removeAt(index)
+                    notifyItemRemoved(index)
+                    notifyItemRangeChanged(index, list.size)
+                }
+            }
+            val media = list[position]
+            val chooseModel = media.chooseModel
+            val path: String
+            path = if (media.isCut && !media.isCompressed) {
+                // 裁剪过
+                media.cutPath
+            } else if (media.isCut || media.isCompressed) {
+                // 压缩过,或者裁剪同时压缩过,以最终压缩过图片为准
+                media.compressPath
+            } else {
+                // 原图
+                media.path
+            }
+            Log.i(TAG, "原图地址::" + media.path)
+            if (media.isCut) {
+                Log.i(TAG, "裁剪地址::" + media.cutPath)
+            }
+            if (media.isCompressed) {
+                Log.i(TAG, "压缩地址::" + media.compressPath)
+                Log.i(TAG, "压缩后文件大小::" + File(media.compressPath).length() / 1024 + "k")
+            }
+            if (media.isToSandboxPath) {
+                Log.i(TAG, "Android Q特有地址::" + media.sandboxPath)
+            }
+            if (media.isOriginal) {
+                Log.i(TAG, "是否开启原图功能::" + true)
+                Log.i(TAG, "开启原图功能后地址::" + media.originalPath)
+            }
+            val duration = media.duration
+            viewHolder.tvDuration.visibility =
+                if (PictureMimeType.isHasVideo(media.mimeType)) View.VISIBLE else View.GONE
+            if (chooseModel == SelectMimeType.ofAudio()) {
+                viewHolder.tvDuration.visibility = View.VISIBLE
+                viewHolder.tvDuration.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    R.drawable.ps_ic_audio,
+                    0,
+                    0,
+                    0
+                )
+            } else {
+                viewHolder.tvDuration.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    R.drawable.ps_ic_video,
+                    0,
+                    0,
+                    0
+                )
+            }
+            viewHolder.tvDuration.text = DateUtils.formatDurationTime(duration)
+            if (chooseModel == SelectMimeType.ofAudio()) {
+                viewHolder.mImg.setImageResource(R.drawable.ps_audio_placeholder)
+            } else {
+                Glide.with(viewHolder.itemView.context)
+                    .load(
+                        if (PictureMimeType.isContent(path) && !media.isCut && !media.isCompressed) Uri.parse(
+                            path
+                        ) else path
+                    )
+                    .centerCrop()
+                    .placeholder(R.color.ps_color_fa632d)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(viewHolder.mImg)
+            }
+            //itemView 的点击事件
+            if (mItemClickListener != null) {
+                viewHolder.itemView.setOnClickListener { v: View? ->
+                    val adapterPosition = viewHolder.absoluteAdapterPosition
+                    mItemClickListener!!.onItemClick(v, adapterPosition)
+                }
+            }
+            if (mItemLongClickListener != null) {
+                viewHolder.itemView.setOnLongClickListener { v: View? ->
+                    val adapterPosition = viewHolder.absoluteAdapterPosition
+                    mItemLongClickListener!!.onItemLongClick(viewHolder, adapterPosition, v)
+                    true
+                }
+            }
+        }
+    }
+
+    private var mItemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(l: OnItemClickListener?) {
+        mItemClickListener = l
+    }
+
+    interface OnItemClickListener {
+        /**
+         * Item click event
+         *
+         * @param v
+         * @param position
+         */
+        fun onItemClick(v: View?, position: Int)
+
+        /**
+         * Open PictureSelector
+         */
+        fun openPicture()
+    }
+
+    private var mItemLongClickListener: PreviewGalleryAdapter.OnItemLongClickListener? = null
+
+    fun setItemLongClickListener(l: PreviewGalleryAdapter.OnItemLongClickListener?) {
+        mItemLongClickListener = l
+    }
+}
