002a36d3c634593167dbf1bc348325c876894e68
chen <1356431628@qq.com>
2022-03-02 10:10:11 +0800

    add： 新增移除粉丝dialog

--- app/src/main/java/cn/copaint/audience/adapter/FansAdapter.kt
+++ app/src/main/java/cn/copaint/audience/adapter/FansAdapter.kt
@@ -2,14 +2,17 @@ package cn.copaint.audience.adapter
 
 import android.graphics.Color
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import cn.copaint.audience.*
+import cn.copaint.audience.databinding.DialogCreatorMoreBinding
+import cn.copaint.audience.databinding.DialogRemoveFanBinding
 import cn.copaint.audience.databinding.ItemFansBinding
 import cn.copaint.audience.utils.AuthingUtils
 import cn.copaint.audience.utils.ToastUtils.toast
+import cn.copaint.audience.utils.dp
 import com.apollographql.apollo3.ApolloClient
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.CoroutineScope
@@ -47,28 +50,68 @@ class FansAdapter(private val activity: FansActivity) : RecyclerView.Adapter<Fan
                 }
             }
             itemBind.unsubscribe.setOnClickListener{
-                val apolloClient = ApolloClient.Builder()
-                    .serverUrl("http://120.78.173.15:20000/query")
-                    .addHttpHeader("Authorization", "Bearer " + AuthingUtils.user.token!!)
-                    .build()
-                CoroutineScope(Dispatchers.IO).launch {
-                    val response = try {
-                        apolloClient.mutation(RemoveFollowerMutation(fans.id)).execute()
-                    }catch ( e: Exception){
-                        Log.e("FansAdapter", e.toString() )
-                        return@launch
-                    }
-                    if ( response.data?.removeFollower == 1){
-                        activity.runOnUiThread{
-//                            activity.fansAdapter.notifyItemChanged(position)
-                            activity.updateUiInfo()
-                        }
-                        toast("删除成功")
-                    }
-                    Log.i("adpater", fans.id+"\n"+ response.data?.removeFollower)
-                }
-
+                onConfirmRemoveDialog(activity,fans.id,fans.photo)
             }
         }
     }
+}
+
+fun onConfirmRemoveDialog(activity: FansActivity, id: String, photo: String?) {
+    var popBind = DialogRemoveFanBinding.inflate(LayoutInflater.from(activity))
+    if (photo == "" || photo?.endsWith("svg") == true) {
+        Glide.with(activity).load(R.drawable.avatar_sample).into(popBind.avatar)
+    } else {
+        Glide.with(activity).load(photo).into(popBind.avatar)
+    }
+
+    // 弹出PopUpWindow
+    var layerDetailWindow = PopupWindow(popBind.root, 300.dp, 240.dp, true)
+    layerDetailWindow.isOutsideTouchable = true
+    val window = activity.window
+
+    // 设置弹窗时背景变暗
+    var layoutParams = window.attributes
+    layoutParams.alpha = 0.4f // 设置透明度
+    window.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+    window.attributes = layoutParams
+
+    // 弹窗消失时背景恢复
+    layerDetailWindow.setOnDismissListener {
+        layoutParams = window.attributes
+        layoutParams.alpha = 1f
+        window.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+        window.attributes = layoutParams
+    }
+
+    layerDetailWindow.showAtLocation(activity.binding.root, Gravity.CENTER, 0, 0)
+
+    popBind.cancelBtn.setOnClickListener {
+        layerDetailWindow.dismiss()
+    }
+    popBind.closeBtn.setOnClickListener {
+        layerDetailWindow.dismiss()
+    }
+    popBind.confirmBtn.setOnClickListener {
+        val apolloClient = ApolloClient.Builder()
+            .serverUrl("http://120.78.173.15:20000/query")
+            .addHttpHeader("Authorization", "Bearer " + AuthingUtils.user.token!!)
+            .build()
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = try {
+                apolloClient.mutation(RemoveFollowerMutation(id)).execute()
+            }catch ( e: Exception){
+                Log.e("FansAdapter", e.toString() )
+                return@launch
+            }
+            if ( response.data?.removeFollower == 1){
+                activity.runOnUiThread{
+                    activity.updateUiInfo()
+                }
+                toast("删除成功")
+            }
+            Log.i("adpater", id+"\n"+ response.data?.removeFollower)
+        }
+        layerDetailWindow.dismiss()
+    }
+
 }
